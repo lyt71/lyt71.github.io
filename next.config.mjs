f2ab6bf7738c441b3f1f79fa0169274822371545
Yiting Liu <lyting71@gmail.com>
2025-05-15 23:58:56 +0800

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
+
