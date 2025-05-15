09d51b45c81e019891930719fcc595a368813ceb
Yiting Liu <lyting71@gmail.com>
2025-05-16 00:56:28 +0800

    简化配置

--- next.config.mjs
+++ next.config.mjs
@@ -11,10 +11,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  basePath: '/lyt71.github.io',
-  assetPrefix: 'https://lyt71.github.io/lyt71.github.io/',
-  trailingSlash: true,
+  }
 };
 
 export default nextConfig;
